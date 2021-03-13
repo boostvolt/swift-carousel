8de749e5328497190e7e78540dc4a0e0bd30f94c
Jan <51777660+Boostvolt@users.noreply.github.com>
2021-03-13 17:33:46 +0100

    Added Xcode project

---
+++ Carousel/Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Jan Kott on 13.03.21.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousel/Carousel/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Carousel
+//
+//  Created by Jan Kott on 13.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
